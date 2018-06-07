edca67670b8b077501072351a650c97fc605618b
yangwenjie <yang@yangwenjiedeMacBook-Pro.local>
2018-06-07 17:33:45 +0800

    first commit

---
+++ src/main/java/com/test/ip_test/IpTestApplication.java
@@ -0,0 +1,12 @@
+package com.test.ip_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpTestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/test/ip_test/TestController.java
@@ -0,0 +1,21 @@
+package com.test.ip_test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author yang
+ */
+@RestController
+@Slf4j
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(HttpServletRequest request) {
+        log.info(request.getRemoteAddr());
+        return "hello";
+    }
+}
